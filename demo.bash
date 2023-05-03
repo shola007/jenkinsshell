47dd50b26fe071518d45fa0d95d4a575409fd420
shola007 <61351843+shola007@users.noreply.github.com>
2023-05-03 08:10:52 +0800

    Update demo.bash

--- demo.bash
+++ demo.bash
@@ -4,7 +4,7 @@
 echo "This is my first script shell writing"
 echo "lists of them"
 echo "Names of Premier league clubs"
-echo "Chelsea FC, Newcastle FC, Manchester United FC, Manchester City FC"
+echo "Chelsea FC, Newcastle FC"
 echo "copyrights reserved"
 echo "I m ready for football"
 echo " new version 2.3.0"
